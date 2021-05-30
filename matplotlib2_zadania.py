12b8d30c7fa2d3530188214ccc9d610a5b98501d
Mmilosnicki <73527089+Mmilosnicki@users.noreply.github.com>
2021-05-30 23:48:45 +0200

    Add files via upload

---
+++ matplotlib2_zadania.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+
+# zadanie 1
+
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+#
+# t = np.linspace(0, 2*np.pi, 100)
+# z = t
+# x = np.sin(z)
+# y = 2 * np.cos(z)
+#
+# ax.plot(x, y, z, label='zadanie 1')
+# plt.legend()
+# plt.show()
